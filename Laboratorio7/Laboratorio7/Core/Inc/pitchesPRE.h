bcb88bad89b2eb4acf66d04362988baef6c44aea
Bryan Morales <mor23283@uvg.edu.gt>
2026-03-24 00:54:22 -0600

    lab7

---
+++ Laboratorio7/Laboratorio7/Core/Inc/pitchesPRE.h
@@ -0,0 +1,116 @@
+/*
+ * pitchesPRE.h
+ *
+ *  Created on: Mar 19, 2026
+ *      Author: edvin
+ */
+
+#ifndef INC_PITCHESPRE_H_
+#define INC_PITCHESPRE_H_
+
+#define NOTE_B0 26942
+
+#define NOTE_C1 25430
+#define NOTE_CS1 24003
+#define NOTE_D1 22656
+#define NOTE_DS1 21384
+#define NOTE_E1 20184
+#define NOTE_F1 19051
+#define NOTE_FS1 17982
+#define NOTE_G1 16972
+#define NOTE_GS1 16020
+#define NOTE_A1 15121
+#define NOTE_AS1 14272
+#define NOTE_B1 13471
+
+#define NOTE_C2 12715
+#define NOTE_CS2 12001
+#define NOTE_D2 11327
+#define NOTE_DS2 10692
+#define NOTE_E2 10091
+#define NOTE_F2 9525
+#define NOTE_FS2 8990
+#define NOTE_G2 8486
+#define NOTE_GS2 8009
+#define NOTE_A2 7560
+#define NOTE_AS2 7135
+#define NOTE_B2 6735
+
+#define NOTE_C3 6357
+#define NOTE_CS3 6000
+#define NOTE_D3 5663
+#define NOTE_DS3 5345
+#define NOTE_E3 5045
+#define NOTE_F3 4762
+#define NOTE_FS3 4495
+#define NOTE_G3 4242
+#define NOTE_GS3 4004
+#define NOTE_A3 3779
+#define NOTE_AS3 3567
+#define NOTE_B3 3367
+
+#define NOTE_C4 3178
+#define NOTE_CS4 2999
+#define NOTE_D4 2831
+#define NOTE_DS4 2672
+#define NOTE_E4 2522
+#define NOTE_F4 2380
+#define NOTE_FS4 2247
+#define NOTE_G4 2121
+#define NOTE_GS4 2002
+#define NOTE_A4 1889
+#define NOTE_AS4 1783
+#define NOTE_B4 1683
+#define NOTE_C5 1588
+#define NOTE_CS5 1499
+#define NOTE_D5 1415
+#define NOTE_DS5 1336
+#define NOTE_E5 1261
+#define NOTE_F5 1190
+#define NOTE_FS5 1123
+#define NOTE_G5 1060
+#define NOTE_GS5 1000
+#define NOTE_A5 944
+#define NOTE_AS5 891
+#define NOTE_B5 841
+
+#define NOTE_C6 794
+#define NOTE_CS6 749
+#define NOTE_D6 707
+#define NOTE_DS6 667
+#define NOTE_E6 630
+#define NOTE_F6 594
+#define NOTE_FS6 561
+#define NOTE_G6 529
+#define NOTE_GS6 500
+#define NOTE_A6 472
+#define NOTE_AS6 445
+#define NOTE_B6 420
+
+#define NOTE_C7 396
+#define NOTE_CS7 374
+#define NOTE_D7 353
+#define NOTE_DS7 333
+#define NOTE_E7 314
+#define NOTE_F7 297
+#define NOTE_FS7 280
+#define NOTE_G7 264
+#define NOTE_GS7 249
+#define NOTE_A7 235
+#define NOTE_AS7 222
+#define NOTE_B7 209
+
+#define NOTE_C8 198
+#define NOTE_CS8 187
+#define NOTE_D8 176
+#define NOTE_DS8 166
+#define NOTE_E8 157
+#define NOTE_F8 148
+#define NOTE_FS8 139
+#define NOTE_G8 132
+#define NOTE_GS8 124
+#define NOTE_A8 117
+#define NOTE_AS8 111
+#define NOTE_B8 104
+
+#endif /* INC_PITCHESPRE_H_ */
